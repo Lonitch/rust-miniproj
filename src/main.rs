f2d549c6d8a6406cd702b77e577870dcb5f34c6c
fit-sizhe <sizhe.liu@fit-foxconn.com>
2025-03-10 14:51:01 -0500

    print-prompt-1

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+  // Uncomment this block to pass the first stage
+  print!("$ ");
+  io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+  // Wait for user input
+  let stdin = io::stdin();
+  let mut input = String::new();
+  stdin.read_line(&mut input).unwrap();
 }
