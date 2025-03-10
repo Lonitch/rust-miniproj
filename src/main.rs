68cf707d8fdaef7a1d7ae7243935c472ea65f774
fit-sizhe <sizhe.liu@fit-foxconn.com>
2025-03-10 16:11:25 -0500

    type-1

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::io::{self, Write};
 
 fn main() {
+  let cmd_headers = ["echo", "type", "exit"];
   // Wait for user input
   let stdin = io::stdin();
   let mut input = String::new();
@@ -9,19 +10,33 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
     stdin.read_line(&mut input).unwrap();
-    // exit cmd
-    if input.contains("exit") {
-      let res = input.split(' ').last().unwrap();
-      match res.trim().parse::<i32>() {
-        Ok(num) => std::process::exit(num),
-        Err(_) => println!("Invalid exit command"),
-      }
-    }
-    if input.contains("echo") {
-      let msg = input.split("echo").last().unwrap().trim();
-      println!("{}", msg);
-    } else {
-      println!("{}: command not found", input.trim());
+    let cmd = input.split(' ').collect::<Vec<&str>>();
+
+    match *cmd.first().unwrap() {
+      "exit" => {
+        let res = cmd.last().unwrap();
+        match res.trim().parse::<i32>() {
+          Ok(num) => std::process::exit(num),
+          Err(_) => println!("Invalid exit command"),
+        }
+      },
+
+      "echo" => {
+        let msg = input.split("echo").last().unwrap().trim();
+        println!("{}", msg);
+      },
+
+      "type" => {
+        let second = cmd.last().unwrap().trim();
+        if cmd_headers.contains(&second) {
+          println!("{second} is a shell builtin");
+        } else {
+          println!("{second}: not found");
+        }
+      },
+      _ => {
+        println!("{}: command not found", input.trim());
+      },
     }
     input.clear();
   }
