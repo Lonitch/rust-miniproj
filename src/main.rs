24d65a265f7b0c5f53bb1dac342d1e437d9cb12c
fit-sizhe <sizhe.liu@fit-foxconn.com>
2025-03-10 15:15:49 -0500

    repl-2

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,12 @@
 use std::io::{self, Write};
 
 fn main() {
-  // Uncomment this block to pass the first stage
-  print!("$ ");
-  io::stdout().flush().unwrap();
-
   // Wait for user input
   let stdin = io::stdin();
   let mut input = String::new();
   loop {
     print!("$ ");
+    io::stdout().flush().unwrap();
     stdin.read_line(&mut input).unwrap();
     println!("{}: command not found", input.trim());
     input.clear();
